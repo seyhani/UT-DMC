6b0666ac4f1b3e2aeb41e4a486c3bb38cc4d6ba9
hadisafari <hadisafari76@gmail.com>
2017-01-01 18:55:03 +0330

    remove publicccc

--- publicccc/scripts/msgbox.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$("#msgok").click(function(event) {
-		$(".msgbox").animate({opacity: 0}, 300, function(){
-			$(".msgbox").addClass("hide").css("opacity", "");
-		});
-	});
-});
