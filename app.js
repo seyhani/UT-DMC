5a1ecbd5d17769ff287483b943438330eac92db7
amir <ahsprim@gmail.com>
2017-01-01 16:46:49 +0330

    User verification added.

--- app.js
+++ app.js
@@ -44,21 +44,24 @@ app.use(flash());
 
 require('./config/passport')(passport);
 
+var baseURL = "/dmc";
+
 app.use(function(req, res, next){
     app.locals.currentUser = req.user;
+    res.locals.baseURL  = "/dmc";
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 });
-var baseURL = "/dmc";
+
 app.use(baseURL+"/", indexRoutes);
-app.use("/admin/", groupRoutes);
-app.use("/admin/", competitionRoutes);
-app.use("/dashboard", dashboardRoutes);
-app.use("/admin/", adminRoutes);
-app.use("/admin/users", userRoutes);
-app.use("/admin/problems", problemRoutes);
-app.use("/admin/tags", tagRoutes);
+app.use(baseURL +"/admin/", groupRoutes);
+app.use(baseURL +"/admin/", competitionRoutes);
+app.use(baseURL +"/dashboard", dashboardRoutes);
+app.use(baseURL +"/admin/", adminRoutes);
+app.use(baseURL +"/admin/users", userRoutes);
+app.use(baseURL +"/admin/problems", problemRoutes);
+app.use(baseURL +"/admin/tags", tagRoutes);
 
 app.use(function(req, res, next) {
     res.status(404).send('Sorry cant find that!');
