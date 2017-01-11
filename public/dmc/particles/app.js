67a457b73702d0de477efc5bbd1d7677d952d2aa
hadisafari <hadisafari76@gmail.com>
2017-01-11 12:49:45 +0330

    ui: change landing particles interactive mode

--- public/dmc/particles/app.js
+++ public/dmc/particles/app.js
@@ -86,11 +86,11 @@ particlesJS('particles-js',
       "events": {
         "onhover": {
           "enable": true,
-          "mode": "repulse"
+          "mode": "grab"
         },
         "onclick": {
           "enable": true,
-          "mode": "push"
+          "mode": "remove"
         },
         "resize": true
       },
