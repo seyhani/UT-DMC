eef075a26830b8a0a8c08748496147b85f454162
hadisafari <hadisafari76@gmail.com>
2017-01-14 13:20:53 +0330

    ui: minor changes in admin login

--- config/test.js
+++ config/test.js
@@ -2,5 +2,6 @@
 // export const baseURL = "";
 // global.baseURL = "";
 // global.host = "http://localhost:3042";
+// global.host = "http://192.168.56.101:3042";
 global.baseURL = "/dmc";
-global.host = "http://acm.ut.ac.ir/dmc";;
+global.host = "http://acm.ut.ac.ir/dmc";
