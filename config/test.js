ec5813033e6f6458f253c860e23582b52000fa9f
hadisafari <hadisafari76@gmail.com>
2017-01-18 23:32:48 +0330

    .

--- config/test.js
+++ config/test.js
@@ -1,7 +1,7 @@
 // export const host = "http://localhost:3042";
 // export const baseURL = "";
-global.baseURL = "";
-global.host = "http://localhost:3042";
+// global.baseURL = "";
+// global.host = "http://localhost:3042";
 // global.host = "http://192.168.56.101:3042";
-// global.baseURL = "/dmc";
-// global.host = "http://acm.ut.ac.ir/dmc";
+global.baseURL = "/dmc";
+global.host = "http://acm.ut.ac.ir/dmc";
